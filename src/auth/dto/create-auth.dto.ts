a51e77988fc0c82527008f7b3034020eeb5778d1
David Daso <daso.timileyin@gmail.com>
2022-07-13 07:57:42 +0100

    create-authDTO validation

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,9 +1,14 @@
-import { IsString } from 'class-validator';
+import { IsString, Length, Matches } from 'class-validator';
 
 export class CreateAuthDto {
   @IsString()
+  @Length(4, 20)
   username: string;
 
   @IsString()
+  @Length(8, 32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n)(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
   password: string;
 }
