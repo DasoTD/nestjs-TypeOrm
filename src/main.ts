493a3849515634d32a1f55ef8129cdd030452652
David Daso <daso.timileyin@gmail.com>
2022-07-13 11:53:37 +0100

    helmet and csurf

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as csurf from 'csurf';
+import * as helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet);
+  app.use(csurf());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
